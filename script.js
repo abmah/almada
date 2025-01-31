092792eadd0437771ca8b344b1e2a35301fc90ca
abmah <ab.mahasnh@gmail.com>
2025-01-31 22:01:41 +0300

    'new'

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 document.querySelectorAll('.swiper-signposting').forEach(function (thisSlider) {
-  const id = thisSlider.id; // make sure your slider element has a unique ID
+  const id = thisSlider.id;
   const slider = document.getElementById(id);
-  const container = slider.parentElement;
+
 
   const swiper = new Swiper(slider, {
-    a11y: {
-      prevSlideMessage: 'Previous slide',
-      nextSlideMessage: 'Next slide',
-    },
-    navigation: {
-      nextEl: container.querySelector('.swiperSignpostingNext'),
-      prevEl: container.querySelector('.swiperSignpostingPrev'),
-    },
+
 
   });
 });
+
